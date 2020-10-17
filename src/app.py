7fc13437401c0abd158854510984f5477ecaddbd
radiv <radiv@3i-mind.com>
2020-10-17 15:50:35 +0300

    add flask and flask_restful modules. add gitignore file. first endpoint.

--- src/app.py
+++ src/app.py
@@ -1,1 +1,16 @@
-print ("hello!")
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Task(Resource):
+
+    def get(self, search):
+        return {"search": search}
+
+
+api.add_resource(Task, '/search/<string:search>')
+
+app.run()
